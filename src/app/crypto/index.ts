b9d976d6c1449837d254e415b9642f13c979d1ff
Christophe Gachiniard <cgachiniard@twitter.com>
2022-05-10 12:36:01 +1200

    add dotenv for env vars

--- src/app/crypto/index.ts
+++ src/app/crypto/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2021 Twitter, Inc.
 // SPDX-License-Identifier: Apache-2.0
-
+import 'dotenv/config'
 import { ApiRouter } from './api'
 import * as config from './config'
 import { stream } from './streamer'
